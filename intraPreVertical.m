a9c94fd7874acaeca277be57e4cde4c33aa48c44
Emily <xuheyao_emily@163.com>
2017-11-30 22:30:28 +0800

    去掉无用参数当前宏块

--- intraPreVertical.m
+++ intraPreVertical.m
@@ -1,4 +1,4 @@
-function result = intraPreVertical(currentBlock, blockSize, preBlock)
+function result = intraPreVertical(blockSize, preBlock)
     result = zeros(blockSize, blockSize);
     for i = 1:blockSize
         for j = 1:blockSize

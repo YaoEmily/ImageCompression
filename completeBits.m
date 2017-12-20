9984e66ffc6f78e38b10ce1facaa2b5e43ff9249
Emily <xuheyao_emily@163.com>
2017-12-20 17:14:36 +0800

    增强补全0功能的兼容性

--- completeBits.m
+++ completeBits.m
@@ -1,7 +1,7 @@
 function [result] = completeBits(bits, num)
     [~, len] = size(bits);
     tmp = '';
-    for i=1:num-1-len
+    for i=1:num-len
         tmp = [tmp, '0'];
     end
     result = [tmp, bits];

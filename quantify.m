bb763b500aa43622f39fd3c50768d64d4253b67b
Emily <xuheyao_emily@163.com>
2017-12-21 18:17:33 +0800

    通过量化步长改变量化损失

--- quantify.m
+++ quantify.m
@@ -1,8 +1,13 @@
-function [result] = quantify(zig, flag)
-    load('quanTable.mat');
-    if flag==0
-        result = round(zig ./ quanTable);
+function [result] = quantify(trans, QP, flag)
+    load('Qstep.mat');
+    if QP>=0 && QP<=51
+        step = Qstep(QP+1, 2);
+        if flag==0
+            result = round(trans ./ step);
+        else
+            result = trans .* step;
+        end
     else
-        result = zig .* quanTable;
+        fprintf('QP is out of range.\n');
     end
 end

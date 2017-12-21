066a5536b04ffdd8b5fecea1412a9f08dfc0bee3
Emily <xuheyao_emily@163.com>
2017-12-21 18:19:43 +0800

    使用标准哈夫曼表进行哈弗曼编码

--- huffman.m
+++ huffman.m
@@ -1,13 +1,30 @@
-function [resultDC, resultAC] = huffman(zig1, zig2, flag)
-    [~, len1] = size(zig1);
-    [~, len2] = size(zig2);
-    load('huffmanDictDC.mat');
-    load('huffmanDictAC.mat');
-    if flag == 0
-        resultDC = huffmanenco(zig1(1:len1/4), DCdict);
-        resultAC = huffmanenco(zig1(len1/4+1:len1), ACdict);
-    else
-        resultDC = huffmandeco(zig1, DCdict);
-        resultAC = huffmandeco(zig2, ACdict);
+function [result] = huffman(bit)
+    load('haffmanTable.mat');
+    [m, ~] = size(bit);
+    sum = 1;
+    result = 0;
+    for i=1:m
+        tmp = bit{i, 1}*16 + bit{i, 2};
+        huf = huffmanTable{tmp, 2};
+        huf = huf(:, 3:6);
+        huf = dec2bin(hex2dec(huf));
+        huf = completeBits(huf, huffmanTable{tmp, 1});
+        [~, len] = size(huf);
+        for j=1:len
+            result(sum) = huf(j)-48;
+            sum = sum + 1;
+        end
+        
+        [~, len] = size(bit{i, 3});
+        for j=1:len
+            result(sum) = bit{i, 3}(j)-48;
+            sum = sum + 1;
+        end
     end
+    
+    % Ìí¼ÓEOB
+    result(sum) = 1;
+    result(sum+1) = 0;
+    result(sum+2) = 1;
+    result(sum+3) = 0;
 end
